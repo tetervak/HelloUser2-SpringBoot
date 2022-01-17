096fa6b5dfea870624a008f84770041738b05578
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-01-17 15:40:12 -0500

    made the index page

---
+++ src/main/java/ca/tetertvak/hellouser2/HelloUser2Application.java
@@ -0,0 +1,13 @@
+package ca.tetertvak.hellouser2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUser2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloUser2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ca/tetertvak/hellouser2/HelloUser2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tetertvak.hellouser2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloUser2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
